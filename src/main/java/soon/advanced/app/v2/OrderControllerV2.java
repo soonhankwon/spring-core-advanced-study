7e44cbc3e2b2e5017bc7114658ff1bfe008954bd
soonhankwon <soonable@gmail.com>
2023-07-25 12:17:07 +0900

    로그추적기 V2 : 적용

---
+++ src/main/java/soon/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package soon.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import soon.advanced.trace.TraceStatus;
+import soon.advanced.trace.hellotrace.HelloTraceV2;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
---
+++ src/main/java/soon/advanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,40 @@
+package soon.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import soon.advanced.trace.TraceId;
+import soon.advanced.trace.TraceStatus;
+import soon.advanced.trace.hellotrace.HelloTraceV2;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+
+    private final HelloTraceV2 trace;
+
+    public void save(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId, "OrderRepository.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

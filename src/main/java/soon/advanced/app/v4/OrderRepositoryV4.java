8c0cc20833ae50d034b37b1ffe96c2cb9ba656b5
soonhankwon <soonable@gmail.com>
2023-08-08 17:44:56 +0900

    템플릿 메서드 패턴 : 적용

---
+++ src/main/java/soon/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,36 @@
+package soon.advanced.app.v4;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import soon.advanced.trace.logtrace.LogTrace;
+import soon.advanced.trace.template.AbstractTemplate;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/soon/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,29 @@
+package soon.advanced.trace.template;
+
+import lombok.RequiredArgsConstructor;
+import soon.advanced.trace.TraceStatus;
+import soon.advanced.trace.logtrace.LogTrace;
+
+@RequiredArgsConstructor
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            // 로직 호출
+            T result = call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}

65ca57ee4ee3bca9e07f5f8c575616d1c27120be
soonhankwon <soonable@gmail.com>
2023-08-08 17:09:34 +0900

    템플릿 메서드 패턴 : 예제

---
+++ src/test/java/soon/advanced/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,19 @@
+package soon.advanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        // start business logic 1
+        call(); //상속
+        // end business logic 1
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    protected abstract void call();
+}
